9255701a16465868a5d5003a9ef70aa7365291e0
Uhrl <estearle@gmail.com>
2017-01-06 17:16:22 -0500

    Fixed token separating and concatenating. Need flag for differentiate string and checksum.

--- test/SerialArduinoTest/SerialArduinoTest.ino
+++ test/SerialArduinoTest/SerialArduinoTest.ino
@@ -5,6 +5,7 @@ void setup()
 
 // serial command buffer
 char buf[255];
+char out[255];
 int buf_offset = 0;
 
 void loop() 
@@ -15,26 +16,30 @@ void loop()
     while(totalBytes > 0) // start loop with number of bytes
     {
       char c = (char)Serial.read(); // read next byte
-      if(c != '\n')
-      {
-        buf[buf_offset++] = c; // store in buffer and continue until newline is found
-      }
-      else // when newline is reached
+      if(c == '\n')
       {
         buf[buf_offset] = '\0'; // null terminator
         // process data
-        char *str = strtok(buf,"*"); //str = roll,pitch,throttle,yaw
-        
+        //Serial.println(buf);
+        char *str = strtok(buf," ,"); //str = roll,pitch,throttle,yaw
+        strcat(out,str);
         while (str != NULL) // loop to print out each token 
         {
-          Serial.print(str);
-          Serial.print("\n");
-          str = strtok (NULL, "*");
+          //Serial.println(str);
+          str = strtok(NULL," ,");  
+          strcat(out,str);  
         }
-        
+        Serial.println(out);
+        buf_offset = 0;
+      }
+      else // when newline is reached
+      {
+        buf[buf_offset++] = c; // store in buffer and continue until newline is found
       }
       totalBytes--;
     }
   }
+  
+  
 
 }
