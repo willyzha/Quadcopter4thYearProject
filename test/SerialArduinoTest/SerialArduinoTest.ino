8c54d1b7127e954156bd8981c0a1daacec0fe3ad
Estearle <estearle@gmail.com>
2017-01-10 00:11:02 -0500

    CheckSum update, got comparing working but need to improve checksum algorithm.

--- test/SerialArduinoTest/SerialArduinoTest.ino
+++ test/SerialArduinoTest/SerialArduinoTest.ino
@@ -6,13 +6,29 @@ void setup()
 // serial command buffer
 char buf[255];
 char out[255];
+char full[255];
+char chs;
+long val[4];
+int counter;
 int buf_offset = 0;
+
+char checkSum(char *str)
+{
+  char sum = 0;
+  int len = strlen(str);
+  for(int i=0;i<len;i++)
+  {
+    sum += str[i];
+  }
+  return sum;
+}
 
 void loop() 
 {
   int totalBytes = Serial.available();
   if(totalBytes > 0) // check to see if data is present
   {
+    counter = 0;
     while(totalBytes > 0) // start loop with number of bytes
     {
       char c = (char)Serial.read(); // read next byte
@@ -21,17 +37,34 @@ void loop()
         buf[buf_offset] = '\0'; // null terminator
         // process data
         //Serial.println(buf);
-        char *str = strtok(buf," ,"); //str = roll,pitch,throttle,yaw
+        char *chk = strtok(buf," *,"); //obtain checkSum
+        Serial.println(chk);
+        char *str = strtok(NULL," *,"); //str = roll,pitch,throttle,yaw
         strcat(out,"Flag: ");
         while (str != NULL) // loop to print out each token 
         {
           //Serial.println(str);
           strcat(out,str);
+          strcat(full,str);
           strcat(out,", ");
+          strcat(full,", ");
+          val[counter] = strtol(str,NULL,16);
           str = strtok(NULL," ,");  
         }
         out[strlen(out)-2] = "\0";
-        Serial.println(out);
+        full[strlen(full)-2] = "\0";
+        chs = checkSum(full);
+        Serial.println(chs);
+        if (chs == *chk)
+        {
+          Serial.println("Success");
+          Serial.println(out);
+        }
+        else
+        {
+          Serial.println("Fail");
+          Serial.println(full);
+        }
         buf_offset = 0;
       }
       else // when newline is reached
@@ -41,8 +74,8 @@ void loop()
       
       totalBytes--;
       out[0] = "\0";
+      full[0] = "\0";
     }
-    Serial.println("Garbage");
+   // Serial.println("Garbage");
   }
 }
-
