0921ed1a0ad246ae5f9f2b1a9ab76a44ad909413
Uhrl <estearle@gmail.com>
2017-01-10 15:44:44 -0500

    Fixed and set to spaces instead of comma for checksum string

--- test/SerialArduinoTest/SerialArduinoTest.ino
+++ test/SerialArduinoTest/SerialArduinoTest.ino
@@ -26,7 +26,6 @@ int checkSum(char *str)
 
 void loop() 
 {
-  int timer = millis();
   int totalBytes = Serial.available();
   if(totalBytes > 0) // check to see if data is present
   {
@@ -34,7 +33,7 @@ void loop()
     while(totalBytes > 0) // start loop with number of bytes
     {
       char c = (char)Serial.read(); // read next byte    
-      if(c == '\n')
+      if(c == '\n') // when \n is reached
       {
         buf[buf_offset] = '\0'; // null terminator
         // process data
@@ -47,17 +46,22 @@ void loop()
           strcat(out,str);
           strcat(full,str);
           strcat(out,", ");
-          strcat(full,", ");
+          strcat(full," ");
           val[counter++] = strtol(str,NULL,16); //saving values of each token as long
           str = strtok(NULL," ,");
         }
         
         out[strlen(out)-2] = "\0";
-        full[strlen(full)-2] = "\0";
-
+        full[strlen(full)-1] = "\0";
+        //Serial.print("CheckSum String: ");
+        Serial.println(full);
         //calculate checksum and convert char chk into int
         chs = checkSum(full);
+        Serial.print("Arduino Calculated: ");
+        Serial.println(chs);
         sscanf(chk,"%d",&compareSum);
+        //Serial.print("Read: ");
+        //Serial.println(chk);
         //compare checksum value with value from python
         if (chs == compareSum)
         {
@@ -70,7 +74,7 @@ void loop()
         }
         buf_offset = 0; //reset buf_offset
       }
-      else // when newline is reached
+      else //c is not \n
       {
         buf[buf_offset++] = c; // store in buffer and continue until newline is found
       }
